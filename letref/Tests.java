9995edb36fa95549a457af16d6e51312baff08df
Casey Klein <clklein@racket-lang.org>
2011-08-24 04:48:26 -0500

    Cleans up tests a bit

--- letref/Tests.java
+++ letref/Tests.java
@@ -6,121 +6,119 @@ public class Tests {
       testEvaluation();
    }
 
-   public static Expr id = new Lam(new Var("x"), new Var("x"));
+   public static Expr id = Lam(Var("x"), Var("x"));
 
    public static Expr unsafeExpr =
-    new Let(new Var("x"),
-            new Ref(id),
-            new Seq(new Set(new Var("x"),
-                            new Lam(new Var("x"), new Bool(true))),
-                    new Incr(new App(new Deref(new Var("x")),
-                                     new Int(2)))));
+    Let(Var("x"),
+        Ref(id),
+        Seq(Set(Var("x"),
+                Lam(Var("x"), Bool(true))),
+                Incr(App(Deref(Var("x")), Int(2)))));
 
    public static void testTyping() {
-      testType(new Int(7), "forall . Int");
-      testNoType(new Incr(id));
+      testType(Int(7), "forall . Int");
+      testNoType(Incr(id));
 
-      testType(new Bool(true), "forall . Bool");
+      testType(Bool(true), "forall . Bool");
 
-      testType(new Ref(new Int(7)), "forall . Ref(Int)");
-      testType(new Incr(new Deref(new Ref(new Int(7)))), "forall . Int");
-      testType(new Set(new Ref(new Int(7)), new Int(8)), "forall . Int");
-      testNoType(new Set(new Ref(new Int(7)), id));
+      testType(Ref(Int(7)), "forall . Ref(Int)");
+      testType(Incr(Deref(Ref(Int(7)))), "forall . Int");
+      testType(Set(Ref(Int(7)), Int(8)), "forall . Int");
+      testNoType(Set(Ref(Int(7)), id));
 
-      testNoType(new Var("x"));
+      testNoType(Var("x"));
 
       testType(id, "forall x0. (x0 -> x0)");
-      testType(new Lam(new Var("x"), new App(id, new Var("x"))),
+      testType(Lam(Var("x"), App(id, Var("x"))),
                "forall x0. (x0 -> x0)");
-      testNoType(new App(new Lam(new Var("x"), 
-                                 new App(new Int(6), new Int(7))),
-                         new Int(8)));
-      testNoType(new App(id, new App(new Int(6), new Int(7))));
-      testNoType(new Lam(new Var("f"),
-                         new App(new Lam(new Var("_"),
-                                         new App(new Var("f"), id)),
-                                 new App(new Var("f"), new Int(7)))));
+      testNoType(App(Lam(Var("x"), 
+                         App(Int(6), Int(7))),
+                     Int(8)));
+      testNoType(App(id, App(Int(6), Int(7))));
+      testNoType(Lam(Var("f"),
+                     App(Lam(Var("_"),
+                             App(Var("f"), id)),
+                         App(Var("f"), Int(7)))));
 
-      testType(new Let(new Var("x"), new Int(7), 
-                       new Incr(new Var("x"))),
+      testType(Let(Var("x"), Int(7), 
+                   Incr(Var("x"))),
                "forall . Int");
-      testNoType(new Let(new Var("x"), new App(new Int(7), new Int(8)),
-                         new Var("x")));
-      testNoType(new Set(new Ref(new Int(0)),
-                         new Let(new Var("T"), new App(new Int(2), new Int(3)),
-                                 new Int(3))));
-      testNoType(new Let(new Var("x"), new Int(7),
-                         new App(new Var("x"), new Int(8))));
-      testType(new Let(new Var("f"), id,
-                       new App(new Lam(new Var("_"),
-                                       new App(new Var("f"), new Int(7))),
-                               new App(new Var("f"), new Var("f"))
+      testNoType(Let(Var("x"), App(Int(7), Int(8)),
+                     Var("x")));
+      testNoType(Set(Ref(Int(0)),
+                         Let(Var("T"), App(Int(2), Int(3)),
+                             Int(3))));
+      testNoType(Let(Var("x"), Int(7),
+                     App(Var("x"), Int(8))));
+      testType(Let(Var("f"), id,
+                   App(Lam(Var("_"),
+                           App(Var("f"), Int(7))),
+                       App(Var("f"), Var("f"))
                                )),
                "forall . Int");
-      testNoType(new Lam(new Var("x"), new App(new Var("x"), new Var("x"))));
-      testNoType(new Lam(new Var("x"),
-                         new Let(new Var("f"),  
-                                 new App(new Var("x"), new Int(0)),
-                                 new Incr(new Var("x")))));
-      testNoType(new App(new App(new Lam(new Var("f"),
-                                         new Lam(new Var("x"),
-                                             new Let(new Var("g"),
-                                                     new Var("f"),
-                                                     new App(new Var("g"), 
-                                                             id)))),
-                                 new Lam(new Var("x"),
-                                         new Incr(new Var("x")))),
-                         new Int(0)));
-      testType(new App(new App(new Lam(new Var("f"),
-                                       new Lam(new Var("x"),
-                                           new Let(new Var("g"),
-                                                   new Var("f"),
-                                                   new App(new Var("g"), 
-                                                           new Var("x"))))),
-                               new Lam(new Var("x"),
-                                       new Incr(new Var("x")))),
-                       new Int(0)),
+      testNoType(Lam(Var("x"), App(Var("x"), Var("x"))));
+      testNoType(Lam(Var("x"),
+                     Let(Var("f"),  
+                         App(Var("x"), Int(0)),
+                         Incr(Var("x")))));
+      testNoType(App(App(Lam(Var("f"),
+                             Lam(Var("x"),
+                                 Let(Var("g"),
+                                     Var("f"),
+                                     App(Var("g"),id)))),
+                         Lam(Var("x"),
+                             Incr(Var("x")))),
+                     Int(0)));
+      testType(App(App(Lam(Var("f"),
+                           Lam(Var("x"),
+                               Let(Var("g"),
+                                   Var("f"),
+                                   App(Var("g"), 
+                                       Var("x"))))),
+                       Lam(Var("x"),
+                           Incr(Var("x")))),
+                   Int(0)),
                 "forall . Int");                        
-      testType(new Lam(new Var("r"),
-                       new Lam(new Var("x"),
-                               new Set(new Var("r"), new Var("x")))),
+      testType(Lam(Var("r"),
+                   Lam(Var("x"),
+                       Set(Var("r"), Var("x")))),
                "forall x1. (Ref(x1) -> (x1 -> Int))");
-      testType(new Lam(new Var("r"),
-                       new Deref(new Var("r"))),
+      testType(Lam(Var("r"),
+                   Deref(Var("r"))),
                "forall x1. (Ref(x1) -> x1)");
-      testNoType(new Let(new Var("c"), id,
-                         new Let(new Var("c"), new Int(1),
-                                 new App(new Var("c"), new Int(2)))));
-      testType(new Lam(new Var("f"),
-                       new Let(new Var("x"), 
-                               new App(new Var("f"), new Int(3)),
-                               new Var("x"))),
+      testNoType(Let(Var("c"), id,
+                     Let(Var("c"), Int(1),
+                     App(Var("c"), Int(2)))));
+      testType(Lam(Var("f"),
+                   Let(Var("x"), 
+                       App(Var("f"), Int(3)),
+                       Var("x"))),
                "forall x1. ((Int -> x1) -> x1)");
 
-      testType(new Lam(new Var("x"), new Lam(new Var("x"), new Var("x"))),
+      testType(Lam(Var("x"), Lam(Var("x"), Var("x"))),
                "forall x0, x1. (x0 -> (x1 -> x1))");
-      testType(new Lam(new Var("x"), new Incr(new Var("x"))),
+      testType(Lam(Var("x"), Incr(Var("x"))),
                "forall . (Int -> Int)");
-      testType(new Lam(new Var("x"), 
-                       new Lam(new Var("y"), 
-                               new App(new Var("x"), new Var("y")))),
+      testType(Lam(Var("x"), 
+                   Lam(Var("y"), 
+                       App(Var("x"), Var("y")))),
                "forall x1, x2. ((x1 -> x2) -> (x1 -> x2))");
-      testNoType(new Lam(new Var("x"), 
-                         new Lam(new Var("y"), 
-                                 new App(new Incr(new Var("x")), 
-                                         new Var("y")))));
-      testNoType(new Lam(new Var("x"), 
-                         new App(new Var("x"), 
-                                 new Var("x"))));
-      testType(new Deref(new Ref(new Int(0))), 
+      testNoType(Lam(Var("x"), 
+                     Lam(Var("y"), 
+                         App(Incr(Var("x")), 
+                             Var("y")))));
+      testNoType(Lam(Var("x"), 
+                     App(Var("x"), 
+                         Var("x"))));
+      testType(Deref(Ref(Int(0))), 
                "forall . Int");
 
-      testType(new Seq(id, new Int(0)), 
+      testType(Seq(id, Int(0)), 
                "forall . Int");
-      testNoType(new Seq(new App(new Int(0), id), new Int(0)));
-      testType(new Lam(new Var("f"),
-                       new Seq(new App(new Var("f"), new Int(0)),
-                               new Var("f"))),
+      testNoType(Seq(App(Int(0), id), Int(0)));
+      testType(Lam(Var("f"),
+                   Seq(App(Var("f"), Int(0)),
+                           Var("f"))),
                "forall x1. ((Int -> x1) -> (Int -> x1))");
 
       // Intentional bug
@@ -128,28 +126,27 @@ public class Tests {
    }
 
    public static void testEvaluation() {
-      testEval(new Incr(new Int(0)), "1");
-      testEval(new Incr(new Incr(new Int(0))), "2");
+      testEval(Incr(Int(0)), "1");
+      testEval(Incr(Incr(Int(0))), "2");
 
-      testEval(new App(new App(new Lam(new Var("x"),
-                                       new Lam(new Var("y"),
-                                               new Var("x"))),
-                               new Incr(new Int(0))),
-                       new Int(0)),
+      testEval(App(App(Lam(Var("x"),
+                           Lam(Var("y"),
+                               Var("x"))),
+                       Incr(Int(0))),
+                   Int(0)),
                "1");
 
-      testEval(new Let(new Var("x"), new Incr(new Int(0)),
-                       new Incr(new Var("x"))),
+      testEval(Let(Var("x"), Incr(Int(0)),
+                   Incr(Var("x"))),
                "2");
 
-      testEval(new Let(new Var("x"), new Ref(new Int(0)),
-                       new Let(new Var("_"),
-                               new Ref(new Int(1)),
-                               new Let(new Var("_"),
-                                       new Set(new Var("x"),
-                                               new Incr(new Deref(
-                                                new Var("x")))),
-                                       new Deref(new Var("x"))))),
+      testEval(Let(Var("x"), Ref(Int(0)),
+                   Let(Var("_"),
+                       Ref(Int(1)),
+                       Let(Var("_"),
+                           Set(Var("x"),
+                               Incr(Deref(Var("x")))),
+                           Deref(Var("x"))))),
                "1");
 
       testStuck(unsafeExpr); 
@@ -187,5 +184,49 @@ public class Tests {
 
    public static void fail(String msg) {
       throw new RuntimeException("test failure: " + msg);
+   }
+
+   public static Int Int(int val) {
+      return new Int(val);
+   }
+
+   public static Deref Deref(Expr body) {
+      return new Deref(body);
+   }
+
+   public static App App(Expr rator, Expr rand) {
+      return new App(rator, rand);
+   }
+
+   public static Incr Incr(Expr body) {
+      return new Incr(body);
+   }
+
+   public static Bool Bool(boolean val) {
+      return new Bool(val);
+   }
+
+   public static Lam Lam(Var param, Expr body) {
+      return new Lam(param, body);
+   }
+
+   public static Set Set(Expr lhs, Expr rhs) {
+      return new Set(lhs, rhs);
+   }
+
+   public static Seq Seq(Expr first, Expr second) {
+      return new Seq(first, second);
+   }
+
+   public static Ref Ref(Expr body) {
+      return new Ref(body);
+   }
+
+   public static Var Var(String name) {
+      return new Var(name);
+   }
+
+   public static Let Let(Var lhs, Expr rhs, Expr body) {
+      return new Let(lhs, rhs, body);
    }
 }
